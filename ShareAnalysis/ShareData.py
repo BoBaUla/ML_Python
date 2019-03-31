8e057d540e66008e2d23548ebbf7a5ad03baef1e
bobaUla <>
2019-03-31 20:03:50 +0200

    added wrapper for panda csv

--- ShareAnalysis/ShareData.py
+++ ShareAnalysis/ShareData.py
@@ -1,3 +1,20 @@
 # -*- coding: utf-8 -*-
+import pandas as pd 
+import RelativePath as rp 
 
-print('shareclass')
+class ShareData:
+    
+    shareData = []
+
+    def __init__(self, dataFile, sep = ';'):
+        self.shareData = pd.read_csv(dataFile, sep = sep)        
+
+    def GetPlainData(self, feature):
+        return self.shareData[feature]
+    
+
+
+
+file = 'WireCard01.csv'
+data = ShareData(rp.GetPathTo(file))
+print(data.GetPlainData('Hoch'))
