1b4fc6b96313c70732fc68d78424bf2b09ebb0f8
bobaUla <>
2019-04-21 21:00:55 +0200

    Added Method to pre-evaluate the dataset

---
+++ ShareAnalysis/EvaluationStrategies/PreEvaluation.py
@@ -0,0 +1,19 @@
+from EvaluationHelper.WeightsHelper import ExpectedValue
+
+def preEvaluateData(data, steps = 1):
+    # print('preEvaluateData')
+    if steps == 0:
+        return []
+    evaluatedData = []
+    lastIndex = len(data)- steps + 1
+    for i in range(lastIndex):
+        subset = data[i: i+steps]    
+
+        buy = ExpectedValue(subset) < subset[steps - 1]
+
+        subset.append(buy)
+        evaluatedData.append(subset)
+    return evaluatedData
+
+
+   
--- ShareAnalysis/EvaluationStrategies/test_PreEvaluation.py
+++ ShareAnalysis/EvaluationStrategies/test_PreEvaluation.py
@@ -2,8 +2,34 @@ from PreEvaluation import preEvaluateData
 
 def test_preEvaluation_Returs0AtNoSteps():
     listToTest = [0]
-    expectedResult = 0
+    expectedResult = []
 
     result = preEvaluateData(listToTest, 0)
+
+    assert result == expectedResult
+
+def test_preEvaluation_SubsetIsSmallerAsSteps_PerfomNoEvaluation():
+    listToTest = [0,2]
+    expectedResult = []
+    
+    result = preEvaluateData(listToTest, 3)
+
+    assert result == expectedResult
+
+def test_preEvaluation_SubsetIsRealSubset_ResultHasCorrectLength():
+    listToTest = [0,2,2,3,5,4,2,1]
+    steps = 4
+    expectedResult = len(listToTest) -  steps + 1
+    
+    result = preEvaluateData(listToTest, steps)
+
+    assert len(result) == expectedResult
+
+def test_preEvaluation_SubsetIsRealSubset_ResultIsCorrect():
+    listToTest = [1,2,3,4,5,1]
+    steps = 4
+    expectedResult = [[1,2,3,4,True],[2,3,4,5,True], [3,4,5,1,False]]
+
+    result = preEvaluateData(listToTest, steps)
 
     assert result == expectedResult
