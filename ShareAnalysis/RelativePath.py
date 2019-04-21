fac53e0cad71426ca33e1a112e343b1450cd9def
bobaUla <>
2019-04-21 10:15:09 +0200

    cleaned project

--- ShareAnalysis/RelativePath.py
+++
@@ -1,9 +0,0 @@
-import os
-
-cwd = os.getcwd()
-relativePath = '..'
-directory = 'Demofiles'
-
-def GetPathTo(file):
-    path = os.path.join( relativePath, directory, file)
-    return os.path.join(cwd,path)
