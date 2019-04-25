06373853c24fe0c542082f0161ed158160d08e06
bobaUla <>
2019-04-25 16:20:23 +0200

    added further evaluation strategies

--- ShareAnalysis/Evaluation/PreEvaluation.py
+++ ShareAnalysis/Evaluation/PreEvaluation.py
@@ -1,6 +1,6 @@
 from Helpers.WeightsHelper import ExpectedValue
 
-def preEvaluateData(data, steps = 1):
+def preEvaluateDataByRisingExpectedValue(data, steps = 1):
     # print('preEvaluateData')
     if steps == 0:
         return []
@@ -9,11 +9,52 @@ def preEvaluateData(data, steps = 1):
     for i in range(lastIndex-1):
         subset = data[i: i+steps]    
 
-        buy = ExpectedValue(subset) < subset[steps - 1]
+        buy = ExpectedValue(subset,1) < subset[steps - 1]
 
         subset.append(buy)
         evaluatedData.append(subset)
     return evaluatedData
 
 
-   
+def preEvaluateDataByFallingExpectedValue(data, steps = 1):
+    # print('preEvaluateData')
+    if steps == 0:
+        return []
+    evaluatedData = []
+    lastIndex = len(data)- steps + 1
+    for i in range(lastIndex-1):
+        subset = data[i: i+steps]    
+
+        buy = ExpectedValue(subset,1) > subset[steps - 1]
+
+        subset.append(buy)
+        evaluatedData.append(subset)
+    return evaluatedData
+
+def EndIntervallEvaluation(data, steps = 1):
+    if steps == 0:
+        return []
+    evaluatedData = []
+    lastIndex = len(data)- steps + 1
+    for i in range(lastIndex-1):
+        subset = data[i: i+steps]    
+
+        buy = subset[0] > subset[steps - 1]
+
+        subset.append(buy)
+        evaluatedData.append(subset)
+    return evaluatedData
+
+def StartIntervallEvaluation(data, steps = 1):
+    if steps == 0:
+        return []
+    evaluatedData = []
+    lastIndex = len(data)- steps + 1
+    for i in range(lastIndex-1):
+        subset = data[i: i+steps]    
+
+        buy = subset[0] > subset[steps - 1]
+
+        subset.append(buy)
+        evaluatedData.append(subset)
+    return evaluatedData
