44bd3731308f98eafe99756824b518e7deb9bca3
bobaUla <>
2019-04-25 17:15:18 +0200

    reworked config.cs

--- ShareAnalysis/Helpers/Config.py
+++ ShareAnalysis/Helpers/Config.py
@@ -2,13 +2,26 @@ import numpy as np
 
 class SimConfig(object):
 
-    def __init__(self, dataPoints = 2000, invest = 1000, fee = 5, steps = 100, init = np.random.randint(50,150), mu = 0, sigma = 0.4, sellAtFactor = 0.08, stopLossFactor = 0.08):
-        self.dataPoints = 2000
-        self.invest = 1000   
-        self.fee = 5
-        self.steps = 100
-        self.init = np.random.randint(50,150)
-        self.mu = 0
-        self.sigma = 0.4
-        self.sellAtFactor = 0.08
-        self.stopLossFactor = 0.08
+    def __init__(
+        self, 
+        dataPoints = 2000, 
+        invest = 1000, 
+        fee = 5, 
+        steps = 100, 
+        init = np.random.randint(50,150), 
+        mu = 0, 
+        sigma = 0.4, 
+        sellAtFactor = 0.08, 
+        stopLossFactor = 0.08,
+        maxGainFactor = 0.2):
+
+        self.dataPoints = dataPoints
+        self.invest = invest
+        self.fee = fee
+        self.steps = steps
+        self.init = init
+        self.mu = mu
+        self.sigma = sigma
+        self.sellAtFactor = sellAtFactor
+        self.stopLossFactor = stopLossFactor
+        self.maxGainFactor = maxGainFactor
