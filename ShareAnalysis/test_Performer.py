dc1f5f26d3f41d1a7e29bb0edc87eb5447275815
bobaUla <>
2019-04-25 21:33:44 +0200

    refactoring

--- ShareAnalysis/test_Performer.py
+++ ShareAnalysis/test_Performer.py
@@ -1,11 +1,8 @@
 import Evaluation.Performer as performer
+from Helpers.Config import SimConfig
 
-money = 10
-cost = 2
+config = SimConfig(invest=10, fee=2, steps=4, sellAtFactor=0, stopLossFactor=0)
 dataSrc = [1,2,3,4,5,1,5]
-steps = 4
-limitFactor = 0
-stopLossFactor = 0
 
 def sellStrategy(price, sellAt, stopLoss):
     return True
@@ -15,14 +12,14 @@ def buyStrategy(buy, canBuy, money, cost, price):
 
 def test_performStrategy_PerformBuyAction_PerformNoSellAction():
     evaluatedData = [[1,2,3,4,True]]
-    result, buyAction, sellAction = performer.performStrategy(money, cost, evaluatedData, dataSrc, steps, sellStrategy=sellStrategy, buyStrategy= buyStrategy)
-    assert result == money - cost
+    result, buyAction, sellAction = performer.performStrategy(config, evaluatedData, dataSrc)
+    assert result == config.invest - config.fee
     assert len(buyAction) == 1
     assert len(sellAction) == 0
     
 def test_performStrategy_PerformBuyAction_PerformSellAction():
     evaluatedData = [[1,2,3,4,True],[2,3,4,5,False]]
-    result, buyAction, sellAction = performer.performStrategy(money, cost, evaluatedData, dataSrc, steps, sellStrategy=sellStrategy, buyStrategy= buyStrategy)
+    result, buyAction, sellAction = performer.performStrategy(config, evaluatedData, dataSrc)
     assert result == 2
     assert len(buyAction) == 1
     assert len(sellAction) == 1
