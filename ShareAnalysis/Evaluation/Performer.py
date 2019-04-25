d422d764c691ff48484a8d10379cd4d32e92f3f4
bobaUla <>
2019-04-25 16:20:56 +0200

    added limit for maxGain

--- ShareAnalysis/Evaluation/Performer.py
+++ ShareAnalysis/Evaluation/Performer.py
@@ -6,12 +6,13 @@ def sellStrategy(price, sellAt, stopLoss):
 def buyStrategy(buy, canBuy, money, cost, price):
     return buy and money - cost > price and canBuy
 
-def performStrategy(money, cost, evaluatedData, dataSrc, steps, 
-    limitFactor = 0, stopLossFactor = 0, 
+def performStrategy(capital, cost, evaluatedData, dataSrc, steps, 
+    limitFactor = 0, stopLossFactor = 0, endFactor = 0,
     sellStrategy = sellStrategy, buyStrategy = buyStrategy):    
     # print('evaluateData')
     price = 0
-    money = money
+    money = capital
+    maxGain = capital * (1+endFactor)
     evaluatedData = evaluatedData
     dataSrc = dataSrc
     steps = steps
@@ -27,23 +28,24 @@ def performStrategy(money, cost, evaluatedData, dataSrc, steps,
         price = dataSrc[index]
         canBuy = True
 
-        if share > 0:
-            if sellStrategy(price, sellAt, stopLoss):
-                money = round((money + price * share) - cost,2)
-                share = 0
-                canBuy = False
-                sellAction.append(np.array([index, price, share, money]))
-                # print('i', stopLossFactor, 'j', limitFactor,'sell', 
-                # 'dataNr', dataNr , round(price,2), round(sellAt,2), round(stopLoss,2), sep = '\t')
+        if money < maxGain:
+            if share > 0:
+                if sellStrategy(price, sellAt, stopLoss):
+                    money = round((money + price * share) - cost,2)
+                    share = 0
+                    canBuy = False
+                    sellAction.append(np.array([index, price, share, money]))
+                    # print('i', stopLossFactor, 'j', limitFactor,'sell', 
+                    # 'dataNr', dataNr , round(price,2), round(sellAt,2), round(stopLoss,2), sep = '\t')
 
-        if buyStrategy(buy, canBuy, money, cost, price):
-            share = int((money - cost)/price) + share
-            money = round(money - share * price - cost,2)
-            sellAt = price * (1 + limitFactor)
-            stopLoss = price * (1 -stopLossFactor)
-            buyAction.append(np.array([index, price, share, money]))
-            # print('i', stopLossFactor, 'j', limitFactor,'buy',
-            # 'dataNr', dataNr , round(price,2), round(sellAt,2), round(stopLoss,2), sep = '\t')
+            if buyStrategy(buy, canBuy, money, cost, price):
+                share = int((money - cost)/price) + share
+                money = round(money - share * price - cost,2)
+                sellAt = price * (1 + limitFactor)
+                stopLoss = price * (1 -stopLossFactor)
+                buyAction.append(np.array([index, price, share, money]))
+                # print('i', stopLossFactor, 'j', limitFactor,'buy',
+                # 'dataNr', dataNr , round(price,2), round(sellAt,2), round(stopLoss,2), sep = '\t')
         dataNr = dataNr + 1
         #print(i, round(price,2), sep='\t')
     gain = round(money + share * price,2)
