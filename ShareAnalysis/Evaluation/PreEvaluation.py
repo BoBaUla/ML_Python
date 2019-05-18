5f9366437a9fc5199ab46aedc25d0307b5e4a518
bobaUla <>
2019-05-18 10:06:16 +0200

    made evaluationmethod injecatable

--- ShareAnalysis/Evaluation/PreEvaluation.py
+++ ShareAnalysis/Evaluation/PreEvaluation.py
@@ -1,6 +1,18 @@
 from Helpers.WeightsHelper import ExpectedValue
 
-def preEvaluateDataByRisingExpectedValue(data, steps = 1):
+def evaluateBuyFallingSituation(subset, steps):
+    return ExpectedValue(subset,1) > subset[steps - 1]
+
+def  evaluateBuyRisingSituation(subset, steps):
+    return ExpectedValue(subset,1) < subset[steps - 1]
+
+def endIntervallEvaluation(subset, steps = 1):
+    return subset[0] > subset[steps - 1]
+
+def startIntervallEvaluation(subset, steps = 1):
+    return subset[0] > subset[steps - 1]
+    
+def preEvaluateData(data, steps = 1, evaluation = evaluateBuyFallingSituation):
     # print('preEvaluateData')
     if steps == 0:
         return []
@@ -9,52 +21,10 @@ def preEvaluateDataByRisingExpectedValue(data, steps = 1):
     for i in range(lastIndex-1):
         subset = data[i: i+steps]    
 
-        buy = ExpectedValue(subset,1) < subset[steps - 1]
+        buy = evaluation(subset, steps)
 
         subset.append(buy)
         evaluatedData.append(subset)
     return evaluatedData
 
 
-def preEvaluateDataByFallingExpectedValue(data, steps = 1):
-    # print('preEvaluateData')
-    if steps == 0:
-        return []
-    evaluatedData = []
-    lastIndex = len(data)- steps + 1
-    for i in range(lastIndex-1):
-        subset = data[i: i+steps]    
-
-        buy = ExpectedValue(subset,1) > subset[steps - 1]
-
-        subset.append(buy)
-        evaluatedData.append(subset)
-    return evaluatedData
-
-def EndIntervallEvaluation(data, steps = 1):
-    if steps == 0:
-        return []
-    evaluatedData = []
-    lastIndex = len(data)- steps + 1
-    for i in range(lastIndex-1):
-        subset = data[i: i+steps]    
-
-        buy = subset[0] > subset[steps - 1]
-
-        subset.append(buy)
-        evaluatedData.append(subset)
-    return evaluatedData
-
-def StartIntervallEvaluation(data, steps = 1):
-    if steps == 0:
-        return []
-    evaluatedData = []
-    lastIndex = len(data)- steps + 1
-    for i in range(lastIndex-1):
-        subset = data[i: i+steps]    
-
-        buy = subset[0] > subset[steps - 1]
-
-        subset.append(buy)
-        evaluatedData.append(subset)
-    return evaluatedData
