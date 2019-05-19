8c5627647e87ac0c2d6cbfd4bffc20f2d2486085
bobaUla <>
2019-05-19 15:00:12 +0200

    refacted tdd

--- ShareAnalysis/Evaluation/PreEvaluation.py
+++ ShareAnalysis/Evaluation/PreEvaluation.py
@@ -1,30 +1,48 @@
 from Helpers.WeightsHelper import ExpectedValue
 
+class EvaluatedData():
+     def __init__(self, subset, performBuyAction, nextValue):
+        self.subset = subset
+        self.performBuyAction = performBuyAction
+        self.nextValue = nextValue
+
 def evaluateBuyFallingSituation(subset, steps):
     return ExpectedValue(subset,1) > subset[steps - 1]
 
-def  evaluateBuyRisingSituation(subset, steps):
+def evaluateBuyRisingSituation(subset, steps):
     return ExpectedValue(subset,1) < subset[steps - 1]
 
 def endIntervallEvaluation(subset, steps = 1):
-    return subset[0] > subset[steps - 1]
+    return subset[0] < subset[steps - 1]
 
 def startIntervallEvaluation(subset, steps = 1):
     return subset[0] > subset[steps - 1]
-    
-def preEvaluateData(data, steps = 1, evaluation = evaluateBuyFallingSituation):
+
+def getSubset(data, i, steps):
+    return data[i: i+steps]   
+
+def calcLastIndex(data, steps):
+    if len(data) < steps:
+        return 0
+
+    return len(data)- steps
+
+def getNextValue(currentIndex, steps, data):
+    nextIndex = steps + currentIndex
+    if nextIndex < len(data):
+        return data[nextIndex]
+    else:
+        return 0
+
+def preEvaluateData(data, steps = 1, evaluation = endIntervallEvaluation):
     # print('preEvaluateData')
     if steps == 0:
         return []
     evaluatedData = []
-    lastIndex = len(data)- steps + 1
-    for i in range(lastIndex-1):
-        subset = data[i: i+steps]    
-
+    lastIndex = calcLastIndex(data, steps)
+    for i in range(lastIndex + 1):    # +1 weil die range von 0 bis lastIndex-1 geht
+        subset = getSubset(data, i, steps)
         buy = evaluation(subset, steps)
-
-        subset.append(buy)
-        evaluatedData.append(subset)
+        nextValue = getNextValue(i, steps, data)
+        evaluatedData.append(EvaluatedData(subset,buy, nextValue))
     return evaluatedData
-
-
--- ShareAnalysis/test_PreEvaluation.py
+++ ShareAnalysis/test_PreEvaluation.py
@@ -1,8 +1,118 @@
-from Evaluation.PreEvaluation import  preEvaluateData
+from Evaluation.PreEvaluation import  *
 # pytest test_PreEvaluation.py
 
-class TestPreEvaluateData(object):
+def evaluationMockMethodTrue(arg1,arg2):
+    return True
 
+class TestCalcLastIndex(object):
+    data = [0,1,2,3,4]
+
+    def test_Returns0(self):
+        val = [0]
+        result = calcLastIndex(val, 2)
+        assert result == 0
+
+    def test_ReturnsCorrectAmount(self):
+        result = calcLastIndex(self.data, 2)
+        assert result == 3
+
+class TestGetNextValue(object):
+    data = [0,1,2,3,4,5,6]
+    steps = 2
+    lastIndex = calcLastIndex(data, steps)
+     
+    def test_getNextValueAtFirstStep(self):
+        result = getNextValue(0,self.steps,self.data)
+        expectedResult = self.data[self.steps]
+
+        assert result == expectedResult
+
+    def test_getNextValueAtSecondStep(self):
+        result = getNextValue(1,self.steps,self.data)
+        expectedResult = self.data[self.steps + 1]
+
+        assert result == expectedResult
+
+    def test_getNextValueAtLastStep(self):
+       
+        result = getNextValue(self.lastIndex,self.steps,self.data)
+        expectedResult = 0
+        
+        assert result == expectedResult
+
+    def test_getNextValueAtPreLastStep(self):
+        result = getNextValue(self.lastIndex - 1,self.steps,self.data)
+        expectedResult = 6
+        
+        assert result == expectedResult
+
+class TestGetSubset(object):
+
+    def test_GetSingleSubset(self):
+        data = [0,1,2,3,4,5,6,7]
+        result = getSubset(data, 0, 3)
+        expectedResult = [0,1,2]
+
+        assert result == expectedResult
+
+class TestPreEvaluateData_CuttingSubsets(object):
+
+    data = [0,1,2]
+
+    def test_GetsCorrectSubsets_AtSteps1(self):
+        result = preEvaluateData(self.data)
+        
+        expectedResult = [EvaluatedData([0],[],0), EvaluatedData([1],[],0), EvaluatedData([2],[],0)]
+
+        for i in range(len(expectedResult)):
+            assert result[i].subset == expectedResult[i].subset
+    
+    def test_GetsCorrectSubsets_AtSteps2(self):
+        result = preEvaluateData(self.data,2)
+        
+        expectedResult = [EvaluatedData([0,1],[],0), EvaluatedData([1,2],[],0)]
+        
+        for i in range(len(expectedResult)):
+            assert result[i].subset == expectedResult[i].subset
+    
+    def test_GetsCorrectSubsets_AtSteps3(self):
+        result = preEvaluateData(self.data,3)
+        
+        expectedResult = [EvaluatedData([0,1,2],[],0)]
+        
+        for i in range(len(expectedResult)):
+            assert result[i].subset == expectedResult[i].subset
+
+class TestPreEvaluationData_SettingNextValue(object):
+    
+    data = [0,1,2]
+
+    def test_GetsCorrectNext_AtSteps1(self):
+        result = preEvaluateData(self.data)
+        
+        expectedResult = [EvaluatedData([0],[],1), EvaluatedData([1],[],2), EvaluatedData([2],[],0)]
+
+        for i in range(len(expectedResult)):
+            assert result[i].nextValue == expectedResult[i].nextValue
+    
+    def test_GetsCorrectNext_AtSteps2(self):
+        result = preEvaluateData(self.data,2)
+        
+        expectedResult = [EvaluatedData([0,1],[],2), EvaluatedData([1,2],[],0)]
+        
+        for i in range(len(expectedResult)):
+            assert result[i].nextValue == expectedResult[i].nextValue
+    
+    def test_GetsCorrectNext_AtSteps3(self):
+        result = preEvaluateData(self.data,3)
+        
+        expectedResult = [EvaluatedData([0,1,2],[],0)]
+        
+        for i in range(len(expectedResult)):
+            assert result[i].nextValue == expectedResult[i].nextValue
+
+class TestPreEvaluation_Misc(object):
+    
     def test_Returs0AtNoSteps(self):
         listToTest = [0]
         expectedResult = []
@@ -11,28 +121,28 @@ class TestPreEvaluateData(object):
 
         assert result == expectedResult
 
-    def test_SubsetIsSmallerAsSteps_PerfomNoEvaluation(self):
-        listToTest = [0,2]
-        expectedResult = []
-        
-        result = preEvaluateData(listToTest, 3)
-
-        assert result == expectedResult
-
     def test_SubsetIsRealSubset_ResultHasCorrectLength(self):
         listToTest = [0,2,2,3,5,4,2,1]
-        steps = 4
-        expectedResult = len(listToTest) -  steps
+        steps = 2
+        expectedResult = calcLastIndex(listToTest, steps) + 1
         
         result = preEvaluateData(listToTest, steps)
 
         assert len(result) == expectedResult
 
-    def test_SubsetIsRealSubset_ResultIsCorrect(self):
+    def test_SubsetIsRealSubset_ResultIsCorrect_MockedTrue(self):
         listToTest = [1,2,3,4,2,1]
         steps = 4
-        expectedResult = [[1,2,3,4,False],[2,3,4,2,True]]
+        expectedResult = [
+            EvaluatedData([1,2,3,4], True, 2),
+            EvaluatedData([2,3,4,2], True, 1),
+            EvaluatedData([3,4,2,1], True, 0)
+            ]
 
-        result = preEvaluateData(listToTest, steps)
 
-        assert result == expectedResult
+        result = preEvaluateData(listToTest, steps, evaluationMockMethodTrue)
+
+        for i in range(len(expectedResult)):
+            assert result[i].nextValue == expectedResult[i].nextValue
+            assert result[i].subset == expectedResult[i].subset
+            assert result[i].performBuyAction == expectedResult[i].performBuyAction
