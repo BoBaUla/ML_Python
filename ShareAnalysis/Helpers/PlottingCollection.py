b233e404f479541e5c71a84676f82b66b69d4dfb
bobaUla <>
2019-05-19 10:08:32 +0200

    added script for plotting histograms

--- ShareAnalysis/Helpers/PlottingCollection.py
+++ ShareAnalysis/Helpers/PlottingCollection.py
@@ -1,1 +1,24 @@
 import numpy as np
+
+def filterZero(data):
+        result = []
+        for dat in data:
+                if dat > 0:
+                        result.append(dat)
+        return result
+
+
+def plotHist(data, plt, step, color = 'blue'):
+        if len(data) == 0:
+                return 
+
+        bins = np.arange(min(data), max(data)+2, step)
+        val = plt.hist(data, bins= bins, color = color)
+        valY = val[0]
+        valX = val[1]
+        avg = np.mean(filterZero(valY))
+        std = np.std(filterZero(valY))
+        plt.plot([min(valX), max(valX)],[avg,avg], 'r-')
+        plt.plot([min(valX), max(valX)],[avg+std,avg+std], 'g-')
+        plt.grid()
+        return valX, valY, avg, std
