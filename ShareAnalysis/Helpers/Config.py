5af94f2b410295971e63fc2de0046d0bff1fb204
bobaUla <>
2019-05-25 11:43:57 +0200

    added explanation to config

--- ShareAnalysis/Helpers/Config.py
+++ ShareAnalysis/Helpers/Config.py
@@ -4,17 +4,17 @@ class SimConfig(object):
 
     def __init__(
         self, 
-        dataPoints = 2000, 
-        invest = 1000, 
-        fee = 5, 
-        steps = 100, 
-        init = np.random.randint(50,150), 
-        mu = 0, 
-        sigma = 0.4, 
-        sellAtFactor = 0.08, 
-        stopLossFactor = 0.08,
-        maxGainFactor = 0.2,
-        maxStrategyRange = 10):
+        dataPoints = 2000, #Anzahl der Werte im Randomwalk
+        invest = 1000,  #Startkapital
+        fee = 5, #Ordergebühren
+        steps = 100, #Länge des für Orderentscheidungen zu untersuchenden Intervalls
+        init = np.random.randint(50,150), #Startpunkt des Randomwalks
+        mu = 0, # Peak der glockenkurve
+        sigma = 0.4, # Wendepunkte der Glockenkurve 1-sigma, 2-sigma 3-sigma Intervalle
+        sellAtFactor = 0.08, # Faktor ab dem der Gewinn verkauft werden soll
+        stopLossFactor = 0.08, # Faktor der maximalen Verlust einer Aktion modelliert
+        maxGainFactor = 0.2, # Faktor der den maximalen Gesamtgewinn beschreibt
+        maxStrategyRange = 10): # Anzahl aller möglichen sellAt/stopLoss Faktoren (wird quadratisch betrachtet)
 
         self.dataPoints = dataPoints
         self.invest = invest
