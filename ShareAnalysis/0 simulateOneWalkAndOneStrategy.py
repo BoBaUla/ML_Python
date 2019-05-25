7803bf8494b8e7b715c7d08c1c95ae6ef73fb28d
bobaUla <>
2019-05-25 13:18:02 +0200

    extended simualtion to evaluate different evaluationstrategies

--- ShareAnalysis/0 simulateOneWalkAndOneStrategy.py
+++ ShareAnalysis/0 simulateOneWalkAndOneStrategy.py
@@ -2,32 +2,72 @@ import matplotlib.pyplot as plt # plotten von daten
 import numpy as np 
 
 from Helpers.Config import SimConfig
-from Evaluation.PreEvaluation import preEvaluateData
+from Evaluation.PreEvaluation import *
 from Evaluation.Performer import performStrategy
 import Helpers.RandomWalkNumberGenerator as rw 
 
-config = SimConfig(sellAtFactor=0.06, stopLossFactor=0.06, mu = np.random.randint(-100,100)/100)
+
+def plotResults(plt, description, data, results):
+    plt.plot(data, 'k-')
+    plt.set_title(description)
+
+    if len(results[1]) > 0:
+        buyX = np.array(results[1])[:,0]
+        buyY = np.array(results[1])[:,1]
+        plt.plot(buyX, buyY, 'go', label = 'buy')
+
+    if len(results[2]) > 0:
+        sellX = np.array(results[2])[:,0]
+        sellY = np.array(results[2])[:,1]
+        plt.plot(sellX, sellY, 'rx', label = 'sell')
+    plt.legend()
+    plt.grid()
+
+    print(description, results[0], sep= '\t')
+
+config = SimConfig(
+    sellAtFactor=0.04, 
+    stopLossFactor=0.06, 
+    steps= 10,
+    mu = np.random.randint(-100,100)/100,
+    sigma =5)
+
 walker = rw.RandomWalker(config.init, config.mu, config.sigma, 0.2)
-
 data = walker.calcWalk(config.dataPoints)
-preparedData = preEvaluateData(data, config.steps)
-[gain, buy, sell] = performStrategy(config, preparedData)
 
+startPreparedData = preEvaluateData(data, config.steps, startIntervallEvaluation)
+startResults = performStrategy(config, startPreparedData)
 
-fig, axs = plt.subplots(2,1)
+endPreparedData =   preEvaluateData(data, config.steps, endIntervallEvaluation)
+endResults = performStrategy(config, endPreparedData)
 
-axs[0].plot(data, 'k-')
-if len(buy) > 0:
-    buyX = np.array(buy)[:,0]
-    buyY = np.array(buy)[:,1]
-    axs[0].plot(buyX, buyY, 'go', label = 'buy')
+fallingPreparedData =   preEvaluateData(data, config.steps, evaluateByFallingSituation)
+fallingResults = performStrategy(config, fallingPreparedData)
 
-if len(sell) > 0:
-    sellX = np.array(sell)[:,0]
-    sellY = np.array(sell)[:,1]
-    axs[0].plot(sellX, sellY, 'rx', label = 'sell')
-axs[0].legend()
-axs[0].grid()
+risingPreparedData =   preEvaluateData(data, config.steps, evaluateBuyRisingSituation)
+risingResults = performStrategy(config, risingPreparedData)
 
-print(gain)
+linearRisingPrepareData =  preEvaluateData(data, config.steps, linearInterpolationRisingEvaluation)
+linearRisingResults = performStrategy(config, linearRisingPrepareData)
+
+linearFallingPrepareData =  preEvaluateData(data, config.steps, linearInterpolationFallingEvaluation)
+linearFallingResults = performStrategy(config, linearFallingPrepareData)
+
+squareMinPrepareData =  preEvaluateData(data, config.steps, squareInterpolation_HasMinimumEvaluation)
+squareMinResults = performStrategy(config, squareMinPrepareData)
+
+squareMaxPrepareData =  preEvaluateData(data, config.steps, squareInterpolation_HasMaximumEvaluation)
+squareMaxResults = performStrategy(config, squareMaxPrepareData)
+
+fig, axs = plt.subplots(2,4)
+
+plotResults(axs[0][0],'startIntervallEvaluation', data, startResults)
+plotResults(axs[1][0],'endIntervallEvaluation', data, endResults)
+plotResults(axs[0][1],'evaluateBuyFallingSituation', data, fallingResults)
+plotResults(axs[1][1],'evaluateBuyRisingSituation', data, risingResults)
+plotResults(axs[0][2],'linearInterpolationRisingEvaluations', data, linearRisingResults)
+plotResults(axs[1][2],'linearInterpolationFallingEvaluation', data, linearFallingResults)
+plotResults(axs[0][3],'squareInterpolation_HasMinimumEvaluation', data, squareMinResults)
+plotResults(axs[1][3],'squareInterpolation_HasMaximumEvaluation', data, squareMaxResults)
 plt.show()
+
