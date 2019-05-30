303abd80f1a2d934204f3cd8fe9d6bd9d5a40765
bobaUla <>
2019-05-30 12:28:03 +0200

    fixed typo

--- ShareAnalysis/0 simulateOneWalkAndOneStrategy.py
+++ ShareAnalysis/0 simulateOneWalkAndOneStrategy.py
@@ -36,7 +36,7 @@ config = SimConfig(
 walker = rw.RandomWalker(config.init, config.mu, config.sigma, 0.2)
 data = walker.calcWalk(config.dataPoints)
 
-startPreparedData = preEvaluateData(data, startIntervallEvaluation, config.stepss)
+startPreparedData = preEvaluateData(data, startIntervallEvaluation, config.steps)
 startResults = performStrategy(config, startPreparedData)
 
 endPreparedData =   preEvaluateData(data, config.steps, endIntervallEvaluation)
