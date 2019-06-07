89820d31dca4f6483c5b8a7b79ded685da1bf152
bobaUla <>
2019-06-07 20:12:50 +0200

    added new preEvaluationStrategy

--- ShareAnalysis/Evaluation/PreEvaluationStrategies.py
+++ ShareAnalysis/Evaluation/PreEvaluationStrategies.py
@@ -1,20 +1,21 @@
 from Helpers.WeightsHelper import ExpectedValue
 from Helpers.mathHelper import linearInterpolation, squareInterpolation
+import numpy as np 
 
-def lastValue(subset, steps):
+def getLastValue(subset, steps):
     return subset[steps - 1]
 
 def evaluateByFallingSituation(subset, steps):
-    return ExpectedValue(subset,1) > lastValue(subset, steps)
+    return ExpectedValue(subset,1) > getLastValue(subset, steps)
 
 def evaluateByRisingSituation(subset, steps):
-    return ExpectedValue(subset,1) < lastValue(subset, steps)
+    return ExpectedValue(subset,1) < getLastValue(subset, steps)
 
 def endIntervallEvaluation(subset, steps = 1):
-    return subset[0] < lastValue(subset, steps)
+    return subset[0] < getLastValue(subset, steps)
 
 def startIntervallEvaluation(subset, steps = 1):
-    return subset[0] > lastValue(subset, steps)
+    return subset[0] > getLastValue(subset, steps)
 
 def linearInterpolationRisingEvaluation(subset, steps = 1):
     return linearInterpolation(subset)[0] > 0
@@ -27,3 +28,17 @@ def squareInterpolation_HasMinimumEvaluation(subset, steps = 1):
 
 def squareInterpolation_HasMaximumEvaluation(subset, steps = 1):
     return squareInterpolation(subset)[0] < 0
+
+memory = [0]
+def buyAtLocalMinimum_Evaluation(subset, steps = 1):
+    minValue = min(subset)
+    lastValue = getLastValue(subset, steps)
+    memory.append(lastValue)
+    if len(memory) < steps:
+        for step in range(steps-1):
+            memory.append(subset[step])
+    
+    meanMemory = np.mean(memory)
+    stdMemory = np.std(memory)
+    return lastValue == minValue and (lastValue < meanMemory - stdMemory)
+
