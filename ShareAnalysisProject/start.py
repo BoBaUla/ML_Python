82152007aa5439e7c4a5a0ba8e196415f22232b5
bobaUla <>
2019-06-09 10:21:53 +0200

    added starting script to enter any evaluation

--- ShareAnalysisProject/start.py
+++ ShareAnalysisProject/start.py
@@ -1,3 +1,21 @@
-from ShareAnalysisScipts import sim_ManyWalksOneStrategy
+from ShareAnalysisScipts.config_Type import SimConfig
+import ShareAnalysisScipts.eva_PreEvaluationStrategies as strategy
+import numpy as np
+from ShareAnalysisScipts import sim_ManyWalksOneStrategy, sim_OneWalkManyStrategies, sim_OneWalkOneBuyStrategy, sim_OneWalkOneStrategy
 
-sim_ManyWalksOneStrategy.Run()
+config = SimConfig(
+    invest = 1000,
+    fee = 5,
+    init = 100,    
+    dataPoints = 1000,
+    mu = np.random.randint(1,100)/100,
+    sigma = np.random.randint(1,5),
+    sellAtFactor = 0.05,
+    stopLossFactor = 0.05,
+    maxStrategyRange = 10,
+    steps = 50
+)
+
+#sim_ManyWalksOneStrategy.Run(config, 100, strategy.buyAtLocalMinimum_Evaluation)
+#sim_OneWalkManyStrategies.Run(config, strategy.buyAtLocalMinimum_Evaluation)
+sim_OneWalkOneBuyStrategy.Run(config, [strategy.buyAtLocalMinimumWithReset_Evaluation, strategy.buyAtLocalMinimum_Evaluation])
