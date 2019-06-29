41e48785a5e090fe39a4f1c8375f871d43b5314d
bobaUla <>
2019-06-29 22:20:59 +0200

    configuration should now be done by integers only

--- ShareAnalysisProject/ShareAnalysisScipts/config_Type.py
+++ ShareAnalysisProject/ShareAnalysisScipts/config_Type.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import hashlib
 
 class SimConfig(object):
 
@@ -9,11 +10,11 @@ class SimConfig(object):
         fee = 5, #Ordergebühren
         steps = 100, #Länge des für Orderentscheidungen zu untersuchenden Intervalls
         init = np.random.randint(50,150), #Startpunkt des Randomwalks
-        mu = 0, # Peak der glockenkurve
-        sigma = 0.4, # Wendepunkte der Glockenkurve 1-sigma, 2-sigma 3-sigma Intervalle
-        sellAtFactor = 0.08, # Faktor ab dem der Gewinn verkauft werden soll
-        stopLossFactor = 0.08, # Faktor der maximalen Verlust einer Aktion modelliert
-        maxGainFactor = 0.2, # Faktor der den maximalen Gesamtgewinn beschreibt
+        mu =  np.random.randint(0,100), # Peak der glockenkurve
+        sigma =  np.random.randint(0,10), # Wendepunkte der Glockenkurve 1-sigma, 2-sigma 3-sigma Intervalle
+        sellAtFactor = 8, # Faktor ab dem der Gewinn verkauft werden soll
+        stopLossFactor = 8, # Faktor der maximalen Verlust einer Aktion modelliert
+        maxGainFactor = 20, # Faktor der den maximalen Gesamtgewinn beschreibt
         maxStrategyRange = 10): # Anzahl aller möglichen sellAt/stopLoss Faktoren (wird quadratisch betrachtet)
 
         self.dataPoints = dataPoints
@@ -21,9 +22,14 @@ class SimConfig(object):
         self.fee = fee
         self.steps = steps
         self.init = init
-        self.mu = mu
-        self.sigma = sigma
-        self.sellAtFactor = sellAtFactor
-        self.stopLossFactor = stopLossFactor
-        self.maxGainFactor = maxGainFactor
+        self.mu = mu / 100
+        self.sigma = sigma / 100
+        self.sellAtFactor = sellAtFactor / 100
+        self.stopLossFactor = stopLossFactor / 100
+        self.maxGainFactor = maxGainFactor / 100
         self.maxStrategyRange = maxStrategyRange
+    
+    def ConfigID(self):
+        
+        val = (str(self.dataPoints)+str(self.invest)+str(self.fee)+str(self.init)+str(self.mu*100)+str(self.sigma*100)+str(self.sellAtFactor*100)+str(self.stopLossFactor*100)+str(self.maxGainFactor*100)+str(self.maxStrategyRange)).replace('.','')
+        return int(val)
--- ShareAnalysisProject/start.py
+++ ShareAnalysisProject/start.py
@@ -4,19 +4,20 @@ import numpy as np
 from ShareAnalysisScipts import  sim_OneWalkManySellingStrategies, sim_OneWalkOneBuyStrategy, sim_OneWalkDifferentBuyingStrategies, sim_ManyWalksOneSellingStrategy
 
 config = SimConfig(
-    invest =10000,
+    invest = 10000,
     fee = 5,
     init = 100,    
-    dataPoints = 2000,
-    mu = np.random.randint(1,100)/100,
+    dataPoints = 20,
+    mu = np.random.randint(1,100),
     sigma = np.random.randint(1,5),
-    sellAtFactor = 0.05,
-    stopLossFactor = 0.05,
+    sellAtFactor = 5,
+    stopLossFactor = 5,
     maxStrategyRange = 10,
-    steps = 50
+
+    steps = 5
 )
 
-#sim_ManyWalksOneSellingStrategy.Run(config = config, simulations = 100, evaluationStrategy = strategy.buyAtLocalMinimum_Evaluation)
-sim_OneWalkManySellingStrategies.Run(config = config, evaluationStrategy = strategy.buyAtLocalMinimum_Evaluation) # Hier stimmt noch was nicht
-#sim_OneWalkOneBuyStrategy.Run(config = config, evaluationStrategies = [strategy.buyAtLocalMinimumWithReset_Evaluation, strategy.buyAtLocalMinimum_Evaluation])
-#sim_OneWalkDifferentBuyingStrategies.Run(config = config)
+sim_ManyWalksOneSellingStrategy.Run(config = config, simulations = 10, evaluationStrategy = strategy.buyAtLocalMinimum_Evaluation)
+# sim_OneWalkManySellingStrategies.Run(config = config, evaluationStrategy = strategy.buyAtLocalMinimumWithReset_Evaluation)
+sim_OneWalkOneBuyStrategy.Run(config = config, evaluationStrategies = [strategy.buyAtLocalMinimumWithReset_Evaluation, strategy.buyAtLocalMinimumWithReset_Evaluation])
+# sim_OneWalkDifferentBuyingStrategies.Run(config = config)
