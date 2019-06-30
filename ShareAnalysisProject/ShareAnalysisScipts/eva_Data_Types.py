6a04f8a06e95994a5ca3ade84680edd8325f995e
bobaUla <>
2019-06-30 21:17:13 +0200

    adjusted save-method from EvaluationResult

--- ShareAnalysisProject/ShareAnalysisScipts/eva_Data_Types.py
+++ ShareAnalysisProject/ShareAnalysisScipts/eva_Data_Types.py
@@ -1,6 +1,8 @@
 import numpy as np
 import json
 import os
+import ShareAnalysisScipts.db_sqlite as sql
+
 
 class EvaluatedData():
      def __init__(self, subset, performBuyAction, nextValue):
@@ -18,27 +20,25 @@ class StratResult:
 class EvaluationResult():
     def __init__(self, data, result, config, algorithm):
         self.algorithm = algorithm
-        self.data = data
+        self.data = json.dumps(data)
         self.gain = result[0]
         if len(result[1]) > 0:
             self.buyAction = json.dumps((np.array(result[1])[:,0]).tolist())
         else:
-            self.buyAction = []
+            self.buyAction = '[0]'
         if len(result[2]) > 0:
             self.sellAction = json.dumps((np.array(result[2])[:,0]).tolist())
         else:
-            self.sellAction = []
-        self.config = json.dumps(config.__dict__)
+            self.sellAction = '[0]'
+        self.config = config
     
     def Save(self):
-        cwd = os.getcwd()
-        target = os.path.join(cwd, self.algorithm)
-        # name = 
-        if not(self.algorithm in os.listdir(target)):
-            os.mkdir()
-        
-        return json.dumps(self.__dict__)
-
+        sql.init_db()
+        method_id   = sql.insert_method( (self.algorithm))
+        config_id   = sql.insert_config(self.config)
+        data_id     = sql.insert_data(self.data, config_id)
+        result_id   = sql.insert_result((self.gain, self.buyAction, self.sellAction))
+        sql.insert_cross(method_id, data_id, result_id)
 
     
         
