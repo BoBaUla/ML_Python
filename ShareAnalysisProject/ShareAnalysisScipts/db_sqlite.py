601c04ad39578526dd4c2e57ea56e9a03a0db2bc
bobaUla <>
2019-06-30 21:16:17 +0200

    added sql-queries to create and insert data to tables

--- ShareAnalysisProject/ShareAnalysisScipts/db_sqlite.py
+++ ShareAnalysisProject/ShareAnalysisScipts/db_sqlite.py
@@ -6,46 +6,47 @@ table_datensatz_name = 'Datensatz'
 fields_datensatz = [
     'id integer PRIMARY KEY autoincrement,', 
     'config_id integer,',
-    'Werte blob null,',    
+    'Werte text,',    
     'FOREIGN KEY (config_id) REFERENCES Konfiguration (id)']
 
 table_configuration_name = 'Konfiguration'
 fields_configuration = [
-    'id integer PRIMARY KEY,', 
+    'id integer PRIMARY KEY autoincrement,'
+    'config_id text,', 
     'Datenpunkte integer,',
     'Guthaben integer,',
     'Transaktionsgebuehr integer,',
     'Schrittlaenge integer,', 
     'Strategienlimit integer,',
     'Kursstart integer,',
-    'mu_rendite decimal,',
-    'sigma_rendite decimal,',
-    'StoppLoss decimal,',
-    'SellAt decimal']
+    'mu_rendite double,',
+    'sigma_rendite double,',
+    'StoppLoss double,',
+    'SellAt double']
 
 table_evaluation_name = 'Auswertungsmethode'
 fields_evaluation = [
     'id integer PRIMARY KEY autoincrement,', 
-    'name blob null'
+    'name text'
 ]
 
 table_results_name = 'Ergebniss'
 field_results = [
     'id integer PRIMARY KEY autoincrement,', 
-    'Scoure decimal,'
-    'Kaeufe blob null,'
-    'Verkaeufe blob null'
+    'Score decimal,'
+    'Kaeufe text,'
+    'Verkaeufe text'
 ]
 
 table_cross_name = 'Messung'
 field_cross = [
     'id integer PRIMARY KEY autoincrement,', 
-    'config_id integer,',
+    'data_id integer,',
     'result_id integer,',
     'method_id integer,',
-    'FOREIGN KEY (config_id) REFERENCES Konfiguration (id),'
-    'FOREIGN KEY (result_id) REFERENCES Ergebniss (id),'
-    'FOREIGN KEY (method_id) REFERENCES Auswertungsmethode (id)'
+    'FOREIGN KEY (data_id) REFERENCES ' +table_datensatz_name+' (id),'
+    'FOREIGN KEY (result_id) REFERENCES ' +table_results_name+' (id),'
+    'FOREIGN KEY (method_id) REFERENCES ' + table_evaluation_name+' (id)'
 ]
 
 
@@ -79,12 +80,88 @@ def create_table_command(table_name, fields):
     cmd = 'CREATE TABLE IF NOT EXISTS ' + table_name + ' ( ' + ''.join( ' {} '.format(i) for i in fields) + ' );'
     return cmd
 
-def init_db():
+def location():
     cwd = os.getcwd()
     dbname = 'data.db'
-    conn = create_connection(os.path.join(cwd, dbname))
+    return os.path.join(cwd, dbname)
+
+def init_db():
+    conn = create_connection(location())
     create_table(conn, create_table_command(table_configuration_name, fields_configuration))
     create_table(conn, create_table_command(table_datensatz_name, fields_datensatz))
     create_table(conn, create_table_command(table_evaluation_name, fields_evaluation))
     create_table(conn, create_table_command(table_results_name, field_results))
     create_table(conn, create_table_command(table_cross_name, field_cross))
+
+def insert_config(config):
+    config_id = config.ConfigID()
+    request = 'Select * from ' + table_configuration_name +' where config_id = ?;'
+    conn = create_connection(location())
+    id = 0
+    with conn:
+        cur = conn.cursor()
+        cur.execute(request, (config_id,))
+        rows = cur.fetchall()
+        if len(rows) == 0:
+            statement = 'insert into  ' + table_configuration_name +'''(
+                config_id, 
+                Datenpunkte, 
+                Guthaben, 
+                Transaktionsgebuehr, 
+                Schrittlaenge, 
+                Strategienlimit,
+                Kursstart,
+                mu_rendite,
+                sigma_rendite,
+                StoppLoss,
+                SellAt) 
+            values (?,?,?,?,?,?,?,?,?,?,?);
+            '''
+            values = config.GetValues()
+            cur.execute(statement, values)
+            id = cur.lastrowid
+        else:
+            id = rows[0][0]
+    return id
+
+def insert_result(result):
+    conn = create_connection(location())
+    id = 0
+    with conn:
+        cur = conn.cursor()
+        cur.execute('insert into ' + table_results_name +'(Score, Kaeufe, Verkaeufe) values (?,?,?);', result)
+        id = cur.lastrowid
+    return id
+
+def insert_method(name):
+    request = 'Select * from ' + table_evaluation_name +' where name = ?;'
+    conn = create_connection(location())
+    id = 0
+    with conn:
+        cur = conn.cursor()
+        cur.execute(request, (name,))
+        rows = cur.fetchall()
+        if len(rows) == 0:
+            cur.execute('insert into ' + table_evaluation_name +'(name) values (?);', (name, ))
+            id = cur.lastrowid
+        else:
+            id = rows[0][0]
+    return id
+
+def insert_data(data, config_id):
+    conn = create_connection(location())
+    id = 0
+    with conn:
+        cur = conn.cursor()
+        cur.execute('insert into ' + table_datensatz_name +'(config_id, Werte) values (?, ?);', (config_id, data))
+        id = cur.lastrowid
+    return id
+
+def insert_cross(method_id, data_id, result_id):
+    conn = create_connection(location())
+    id = 0
+    with conn:
+        cur = conn.cursor()
+        cur.execute('insert into ' + table_cross_name +'(data_id, result_id, method_id) values (?,?,?);', (method_id, data_id, result_id))
+        id = cur.lastrowid
+    return id
