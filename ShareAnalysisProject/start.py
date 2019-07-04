c54f1f322d79de58427768d50ad037dcd3e8a111
bobaUla <>
2019-07-04 10:39:15 +0200

    Rename Config to TradeConfig

--- ShareAnalysisProject/start.py
+++ ShareAnalysisProject/start.py
@@ -1,22 +1,24 @@
-from ShareAnalysisScipts.config_Type import SimConfig
+from ShareAnalysisScipts.config_Type import TradeConfig
 import ShareAnalysisScipts.eva_PreEvaluationStrategies as strategy
 import numpy as np
 from ShareAnalysisScipts import  sim_OneWalkManySellingStrategies, sim_OneWalkOneBuyStrategy, sim_OneWalkDifferentBuyingStrategies, sim_ManyWalksOneSellingStrategy
 
-config = SimConfig(
+config = TradeConfig(
     invest = 10000,
     fee = 5,
     init = 100,    
-    dataPoints = 20,
+    dataPoints = 1000,
     mu = np.random.randint(1,100),
     sigma = np.random.randint(1,5),
     sellAtFactor = 5,
     stopLossFactor = 5,
-    maxStrategyRange = 10,
-    steps = 5
+    maxStrategyRange = 5,
+    steps = 50
 )
 
-sim_ManyWalksOneSellingStrategy.Run(config = config, simulations = 10, evaluationStrategy = strategy.buyAtLocalMinimum_Evaluation)
-# sim_OneWalkManySellingStrategies.Run(config = config, evaluationStrategy = strategy.buyAtLocalMinimumWithReset_Evaluation)
-sim_OneWalkOneBuyStrategy.Run(config = config, evaluationStrategies = [strategy.buyAtLocalMinimumWithReset_Evaluation, strategy.buyAtLocalMinimumWithReset_Evaluation])
-# sim_OneWalkDifferentBuyingStrategies.Run(config = config)
+# sim_ManyWalksOneSellingStrategy.Run(config = config, simulations = 10, evaluationStrategy = strategy.buyAtLocalMinimum_Evaluation, save=True)
+# sim_OneWalkManySellingStrategies.Run(config = config, evaluationStrategy = strategy.buyAtLocalMinimumWithReset_Evaluation, save = True)
+
+strategies = [strategy.buyAtLocalMinimumWithReset_Evaluation, strategy.startIntervallEvaluation, strategy.squareInterpolation_HasMaximumEvaluation]
+sim_OneWalkOneBuyStrategy.Run(config = config, evaluationStrategies = strategies, save= True)
+# sim_OneWalkDifferentBuyingStrategies.Rusn(config = config)
